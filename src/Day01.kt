d8e671887ffc36ebde3f95c206a9974f9bfbb70c
Laurentiu <delcea.laurentiu@gmail.com>
2021-12-01 16:16:32 +0200

    Day 1 solutions

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,25 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>) : Int {
+        return countIncreases(input.map { it.toInt() })
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val movingSums = input.mapIndexed { index, element ->
+            if (index == 0 || index == input.size - 1) 0
+            else input[index - 1].toInt() + element.toInt() + input[index + 1].toInt()
+        }
+        return countIncreases(movingSums.subList(1, movingSums.size - 1))
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+private fun countIncreases(elements : List<Int>) : Int {
+    return elements.mapIndexed { index, element ->
+        if (index > 0 && element > elements[index - 1])
+            1
+        else 0
+    }.sum()
 }
